565c844000ae8ac0540f89f17ee72b0d423f9e3e
ilyesselmi454-cmd <ilyesselmi454@gmail.com>
2026-03-28 01:30:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function generate() {
   let name = document.getElementById("name").value;
   let job = document.getElementById("job").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
   let desc = document.getElementById("desc").value;
   let skills = document.getElementById("skills").value.split(",");
   let exp = document.getElementById("exp").value;
 
   document.getElementById("pname").innerText = name;
   document.getElementById("pjob").innerText = job;
+  document.getElementById("pcontact").innerText = email + " | " + phone;
   document.getElementById("pdesc").innerText = desc;
   document.getElementById("pexp").innerText = exp;
 
@@ -15,6 +18,7 @@ function generate() {
 
   skills.forEach(skill => {
     let div = document.createElement("div");
+    div.draggable = true;
 
     div.innerHTML = `
       <p>${skill}</p>
@@ -27,31 +31,33 @@ function generate() {
   });
 
   let file = document.getElementById("photo").files[0];
-  if (file) {
-    let reader = new FileReader();
-    reader.onload = e => {
-      document.getElementById("pimage").src = e.target.result;
+  if(file){
+    let reader=new FileReader();
+    reader.onload=e=>{
+      document.getElementById("pimage").src=e.target.result;
     };
     reader.readAsDataURL(file);
   }
 }
 
-/* TEMPLATE SWITCH */
-function changeTemplate() {
-  let cv = document.getElementById("cv");
-  let template = document.getElementById("template").value;
-
-  cv.className = "cv " + template;
+/* TEMPLATE */
+function changeTemplate(){
+  let cv=document.getElementById("cv");
+  cv.className="cv "+document.getElementById("template").value;
 }
 
 /* PDF */
-function downloadPDF() {
-  let element = document.getElementById("cv");
-
-  html2pdf().set({
-    margin: 0,
-    filename: 'cv.pdf',
-    html2canvas: { scale: 3 },
-    jsPDF: { unit: 'mm', format: 'a4' }
-  }).from(element).save();
+function downloadPDF(){
+  html2pdf().from(document.getElementById("cv")).save();
 }
+
+/* AUTO FILL */
+function autoFill(){
+  document.getElementById("desc").value="Développeur web passionné...";
+  document.getElementById("skills").value="HTML,CSS,JS";
+  document.getElementById("exp").value="Projets web personnels";
+}
+
+/* SUGGESTIONS */
+document.getElementById("desc").onfocus=()=>descHelp.innerText="Parle de toi...";
+document.getElementById("desc").onblur=()=>descHelp.innerText="";
