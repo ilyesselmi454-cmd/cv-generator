ac358a0053b968fdd5fdc44135ebf05459a6f247
ilyesselmi454-cmd <ilyesselmi454@gmail.com>
2026-03-28 01:57:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
 function generate() {
-
-  let cv = document.getElementById("cv");
-  cv.className = "cv " + template.value;
-
-  let jobText = job.value.toLowerCase();
-
-  // RESET
-  cv.classList.remove("male","female","dev-theme","marketing-theme","design-theme");
-
-  // GENRE
-  if (gender.value === "female") {
-    cv.classList.add("female");
-  } else {
-    cv.classList.add("male");
-  }
-
-  // METIER
-  if (jobText.includes("dev")) {
-    cv.classList.add("dev-theme");
-  } else if (jobText.includes("marketing")) {
-    cv.classList.add("marketing-theme");
-  } else if (jobText.includes("design")) {
-    cv.classList.add("design-theme");
-  }
-
   pname.innerText = name.value;
   pjob.innerText = job.value;
   pemail.innerText = email.value;
@@ -52,23 +27,11 @@ function generate() {
   }
 }
 
-/* TEMPLATE */
-function changeTemplate(){
-  cv.className = "cv " + template.value;
-}
-
-/* PDF */
 function downloadPDF(){
   html2pdf().set({
-    margin:5,
+    margin:0,
     filename:'cv-pro.pdf',
     html2canvas:{scale:3},
     jsPDF:{unit:'mm',format:'a4'}
   }).from(cv).save();
 }
-
-/* SUGGESTIONS */
-desc.onfocus=()=>descHelp.innerText="Ex: Développeur motivé avec projets modernes...";
-desc.onblur=()=>descHelp.innerText="";
-skills.onfocus=()=>skillsHelp.innerText="Ex: HTML, CSS, JavaScript";
-skills.onblur=()=>skillsHelp.innerText="";
